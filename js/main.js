2ebcd452069ae591cbbaa492e0d0f1521080200f
Victor Enamorado <vicnamo97@gmail.com>
2023-09-13 17:34:52 -0600

    se logro adaptar el fondo reactivo, aun esta espuesto a cambios y mejoas

--- js/main.js
+++ js/main.js
@@ -60,3 +60,11 @@ function myFunction() {
   triangle.style.strokeDashoffset = length - draw;
   };
 
+  const curser = document.querySelector(".curser");
+  document.addEventListener('mousemove', function(e){
+      let X = e.clientX;
+      let Y = e.clientY;
+  
+      curser.style.left = X + "px";
+      curser.style.top = Y + "px";
+  })
